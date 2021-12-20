b59880fa6643a5032622f720870ccf6abc14e746
Mariela Goncalves Colaco <marielagcw@hotmail.com>
2021-12-20 18:21:11 -0300

    Actualización CV

---
+++ JS/app.js
@@ -0,0 +1,19 @@
+/* -------------------------------------------------------------------------- */
+/*                               Juego de la Oca                              */
+/* -------------------------------------------------------------------------- */
+
+/* ------------------- Indicaciones para armar el tablero ------------------- */
+// 1. Colocarle número a cada una de las celdas
+// 2. Agregar contacto e información sobre el juego al inicio en .bienvenida
+// 3. Armar el dado o los dados para jugar:
+//  a. Los dados tienen que ser aleatorios pero caer en las celdas que tienen contenido:
+//  b. Las secciones son:
+//  ♥ Sobre mí → objetivo personal #aboutme (OCA)
+//  ♥ Docente → Experiencia laboral como docente #teacher (DADOS)
+//  ♥ Contadora → Experiencia laboral como contadora #accountant (LABERINTO)
+//  ♥ Estudios → Certificados universitarios y técnicos #studies (POSADA)
+//  ♥ Cursos → Certificados de cursos y capacitaciones #curses (OCA)
+//  ♥ Habilidades → conocimientos y softskills #skills (PUENTE)
+//  ♥ Proyectos → mis páginas y proyectos personales #projects (PUENTE)
+
+
